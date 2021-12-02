51daef6ae0ab4e3835d56d52c918bf24c90d76cf
Edmundo Paulino <manyymoore@gmail.com>
2021-12-02 10:13:29 -0300

    feat: ✨ Get fibonacci number by entering 0-indexed position

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,44 @@
+struct Fibonacci {
+    curr: u32,
+    next: u32,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_next = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new_next;
+
+        Some(self.curr)
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci { curr: 0, next: 1 }
+}
+
+pub fn get(position: usize) -> u32 {
+    fibonacci().skip(position).take(1).collect::<Vec<u32>>()[0]
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn first_four() {
+        assert_eq!(fibonacci().take(4).collect::<Vec<u32>>(), vec![1, 1, 2, 3]);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(fibonacci().skip(4).take(1).collect::<Vec<u32>>(), vec![5]);
+    }
+
+    #[test]
+    fn fifth_from_pub() {
+        assert_eq!(get(4), 5);
     }
 }
